4a2d8408baefdd3813b4d36cf94a86512e474eb0
Piquel <63727792+PiquelChips@users.noreply.github.com>
2025-03-19 18:10:58 +0100

    fix: stop using pointers in methods

--- fifteen/fifteen.go
+++ fifteen/fifteen.go
@@ -9,46 +9,46 @@ var Solution FifteenVertex = [][]int{
 	{13, 14, 15, 0},
 }
 
-func (vertex *FifteenVertex) GetPotential(*FifteenVertex) {
+func (vertex FifteenVertex) GetPotential(*FifteenVertex) {
 }
 
-func (vertex *FifteenVertex) GetNextVertex(edge FifteenVertex) FifteenVertex {
+func (vertex FifteenVertex) GetNextVertex(edge FifteenEdge) FifteenVertex {
 	return make(FifteenVertex, 0)
 }
 
-func (vertex *FifteenVertex) GetAllEdges() []FifteenEdge {
-    zeroX, zeroY := vertex.getZero()
-    edges := []FifteenEdge{}
+func (vertex FifteenVertex) GetAllEdges() []FifteenEdge {
+	zeroX, zeroY := vertex.getZero()
+	edges := []FifteenEdge{}
 
-    if zeroX < 3 {
-        edge := FifteenEdge{number: (*vertex)[zeroY][zeroX+1], direction: left}
-        edges = append(edges, edge)
-    }
-    
-    // down
-    if zeroY > 0 {
-        edge := FifteenEdge{number: (*vertex)[zeroY-1][zeroX], direction: down}
-        edges = append(edges, edge)
-    }
+	if zeroX < 3 {
+		edge := FifteenEdge{number: vertex[zeroY][zeroX+1], direction: left}
+		edges = append(edges, edge)
+	}
 
-    // up
-    if zeroY < 3 {
-        edge := FifteenEdge{number: (*vertex)[zeroY+1][zeroX], direction: up}
-        edges = append(edges, edge)
-    }
+	// down
+	if zeroY > 0 {
+		edge := FifteenEdge{number: vertex[zeroY-1][zeroX], direction: down}
+		edges = append(edges, edge)
+	}
 
-    if zeroX > 0 {
-        edge := FifteenEdge{number: (*vertex)[zeroY][zeroX-1], direction: right}
-        edges = append(edges, edge)
-    }
+	// up
+	if zeroY < 3 {
+		edge := FifteenEdge{number: vertex[zeroY+1][zeroX], direction: up}
+		edges = append(edges, edge)
+	}
+
+	if zeroX > 0 {
+		edge := FifteenEdge{number: vertex[zeroY][zeroX-1], direction: right}
+		edges = append(edges, edge)
+	}
 
 	return edges
 }
 
 // Return x and y coords of the zero
-func (vertex *FifteenVertex) getZero() (int, int) {
-	for y := range *vertex {
-		for x, number := range (*vertex)[y] {
+func (vertex FifteenVertex) getZero() (int, int) {
+	for y := range vertex {
+		for x, number := range vertex[y] {
 			if number == 0 {
 				return x, y
 			}
@@ -58,9 +58,9 @@ func (vertex *FifteenVertex) getZero() (int, int) {
 	return -1, -1
 }
 
-func (vertex *FifteenVertex) Print() {
-	for y := range *vertex {
-		for _, num := range (*vertex)[y] {
+func (vertex FifteenVertex) Print() {
+	for y := range vertex {
+		for _, num := range (vertex)[y] {
 			if num < 10 {
 				fmt.Printf("%d  ", num)
 			} else {
@@ -70,13 +70,14 @@ func (vertex *FifteenVertex) Print() {
 
 		fmt.Printf("\n")
 	}
+	fmt.Printf("\n")
 }
 
-func (edge *FifteenEdge) Print() {
-    space := ""
-    if edge.number < 10 {
-        space = " "
-    }
+func (edge FifteenEdge) Print() {
+	space := ""
+	if edge.number < 10 {
+		space = " "
+	}
 
-    fmt.Printf("%d,%s %s\n", edge.number, space, edge.direction)
+	fmt.Printf("%d,%s %s\n", edge.number, space, edge.direction)
 }
